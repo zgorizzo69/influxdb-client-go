e6fa2d641f1bd7358065f950c6254aa221c59948
George MacRorie <me@georgemac.com>
2019-10-21 12:02:52 +0100

    Add opentracing collection from context to outbound requests

--- http.go
+++ http.go
@@ -5,7 +5,28 @@ import (
 	"net"
 	"net/http"
 	"time"
+
+	"github.com/opentracing/opentracing-go"
 )
+
+// RoundTripperFunc is a function which can be used as a http.RoundTripper
+type RoundTripperFunc func(req *http.Request) (*http.Response, error)
+
+// RoundTrip delegates to the callee RoundTripperFunc
+func (r RoundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) { return r(req) }
+
+func opentracingRoundTripper(r http.RoundTripper) http.RoundTripper {
+	return RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+		if span := opentracing.SpanFromContext(req.Context()); span != nil {
+			opentracing.GlobalTracer().Inject(
+				span.Context(),
+				opentracing.HTTPHeaders,
+				opentracing.HTTPHeadersCarrier(req.Header))
+		}
+
+		return r.RoundTrip(req)
+	})
+}
 
 func newTransport() *http.Transport {
 	return &http.Transport{
@@ -19,7 +40,7 @@ func newTransport() *http.Transport {
 func defaultHTTPClient() *http.Client {
 	return &http.Client{
 		Timeout:   time.Second * 20,
-		Transport: newTransport(),
+		Transport: opentracingRoundTripper(newTransport()),
 	}
 }
 
@@ -27,12 +48,12 @@ func defaultHTTPClient() *http.Client {
 func HTTPClientWithTLSConfig(conf *tls.Config) *http.Client {
 	return &http.Client{
 		Timeout: time.Second * 20,
-		Transport: &http.Transport{
+		Transport: opentracingRoundTripper(&http.Transport{
 			Dial: (&net.Dialer{
 				Timeout: 5 * time.Second,
 			}).Dial,
 			TLSHandshakeTimeout: 5 * time.Second,
 			TLSClientConfig:     conf,
-		},
+		}),
 	}
 }
